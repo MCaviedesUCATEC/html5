f78fcddd615a2eef50af30e891209c551b2b336c
MCaviedesUCATEC <miguel.caviedes@ucatec.edu.bo>
2025-03-10 22:01:17 -0400

    conbios
    a

--- ejemplos geolocalizacion/ejemplo01G/js/googlemaps.js
+++ ejemplos geolocalizacion/ejemplo01G/js/googlemaps.js
@@ -1,20 +1,49 @@
-function iniciar() {
-    var elemento = document.getElementById("obtener");
-    elemento.addEventListener("click", obtenerubicacion);
-}
+ /** @type {HTMLElement}*/
+ var posElt;
+ /** @type {HTMLElement} */
+ var posLinkElt;
 
-function obtenerubicacion() {
-    navigator.geolocation.getCurrentPosition(mostrar, mostrarerror);
-}
 
-function mostrar(posicion) {
-    var ubicacion = document.getElementById("ubicacion");
-    var mapurl = "http://maps.google.com/maps/api/staticmap?center=" + posicion.coords.latitude + "," + posicion.coords.longitude + "&zoom=12&size=400x400&sensor=false&markers=" + posicion.coords.latitude + "," + posicion.coords.longitude; 
-    ubicacion.innerHTML = '<img src="' + mapurl + '">';
-}
+ window.addEventListener('load', function(){
+     posElt = document.getElementById('Pos');
+     posLinkElt = document.querySelector('#PosLink > a');
 
-function mostrarerror(error) {
-    alert("Error: " + error.code + " " + error.message);
-}
+     // if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(geoposOK, geoposKO);
+     // }
+     // else {
+     // }
+ });
 
-window.addEventListener("load", iniciar);
+ /** @param {GeolocationPosition} pos */
+ function geoposOK(pos) {
+     //Obtenemos latitud y longitud
+     var lat = pos.coords.latitude;
+     var long = pos.coords.longitude;
+     //Mostramos la posición
+     posElt.textContent = `${lat}, ${long}`;
+     //generamos enlace a la posición
+     posLinkElt.href = `https://maps.google.com/?q=${lat},${long}`;
+     posLinkElt.textContent = 'Mostrar tu posición en un mapa';
+ }
+
+ /** @param {GeolocationPositionError} err */
+ function geoposKO(err) {
+     console.warn(err.message);
+     let msg;
+     switch(err.code) {
+         case err.PERMISSION_DENIED:
+             msg = "No nos has dado permiso para obtener tu posición";
+             break;
+         case err.POSITION_UNAVAILABLE:
+             msg = "Tu posición actual no está disponible";
+             break;
+          case err.TIMEOUT:
+              msg = "No se ha podido obtener tu posición en un tiempo prudencial";
+              break;
+          default:
+              msg = "Error desconocido";
+              break;
+     }
+     posElt.textContent = msg;
+ }
